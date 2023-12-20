e671212f54f9979aac7a32fb601b74a61378669b
Eduardo Soares <edurbs@gmail.com>
2023-12-20 17:40:42 -0300

    create project

---
+++ src/test/java/br/nom/soares/eduardo/bible2meps/bible2meps/Bible2mepsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.nom.soares.eduardo.bible2meps.bible2meps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bible2mepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
