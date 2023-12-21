fbf4b8556bc2f77ae8c79a6474d6211bbe199b49
Eduardo Soares <edurbs@gmail.com>
2023-12-21 08:27:39 -0300

    fix get clean text

--- src/main/java/br/nom/soares/eduardo/bible2meps/bible2meps/application/format/YouVersionFormatChapter.java
+++ src/main/java/br/nom/soares/eduardo/bible2meps/bible2meps/application/format/YouVersionFormatChapter.java
@@ -1,7 +1,5 @@
 package br.nom.soares.eduardo.bible2meps.bible2meps.application.format;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
@@ -12,7 +10,7 @@ import lombok.Getter;
 @Getter
 public class YouVersionFormatChapter {
     
-    private Document page;   
+    private Element page;   
 
     public void execute(){
         Element chapter = extractChapter(page);
@@ -21,7 +19,7 @@ public class YouVersionFormatChapter {
     }
 
     private void setPageWithFinalFormatedText(Element chapterOnly) {
-        this.page = Jsoup.parse("<html><body>"+chapterOnly.html()+"</body></html>");
+        this.page = chapterOnly;
     }
 
     protected void removeUnwantedNotes(Element element) {
@@ -31,7 +29,7 @@ public class YouVersionFormatChapter {
         }       
     }
 
-    protected Element extractChapter(Document page){
+    protected Element extractChapter(Element page){
         return page.selectFirst("div.ChapterContent_chapter__uvbXo");
     }
 
