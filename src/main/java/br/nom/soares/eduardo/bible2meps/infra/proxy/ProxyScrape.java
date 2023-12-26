3ec846256181dda748b5fb8d477d194077b6afdd
Eduardo Soares <edurbs@gmail.com>
2023-12-26 00:55:21 -0300

    add manual proxy list

--- src/main/java/br/nom/soares/eduardo/bible2meps/infra/proxy/ProxyScrape.java
+++ src/main/java/br/nom/soares/eduardo/bible2meps/infra/proxy/ProxyScrape.java
@@ -1,5 +1,9 @@
 package br.nom.soares.eduardo.bible2meps.infra.proxy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -25,6 +29,24 @@ public class ProxyScrape implements ProxyListServer {
         return proxies.get(index);
     }
 
+    private void addProxyFromTxtFile() {
+        // read a file from resources and add each line as proxy to the proxies list
+        try {
+            InputStream inputStream =
+                    ProxyScrape.class.getClassLoader().getResourceAsStream("proxyList.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                proxies.add(new Proxy(parts[0], Integer.parseInt(parts[1])));
+            }
+            reader.close();
+        } catch (IOException e) {
+            // handle exception
+        }
+
+    }
+
     public void removeProxy(Proxy proxy) {
         proxies.remove(proxy);
         // TODO make tests
@@ -41,6 +63,7 @@ public class ProxyScrape implements ProxyListServer {
             proxies.add(proxy);
         }
         proxies.add(new Proxy("", 0));
+        addProxyFromTxtFile();
     }
 
     private String getText(String apiUrl) {
